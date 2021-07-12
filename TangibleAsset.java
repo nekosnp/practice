1a4747b9a5560e58d67f3c0e95548b6427632a30
nekosnp <kento6021991@gmail.com>
2021-07-12 13:32:26 +0900

    Create Book.java

--- TangibleAsset.java
+++ TangibleAsset.java
@@ -7,7 +7,7 @@ public abstract class TangibleAsset{
     this.price = price;
     this.color = color;
   }
-  public String getName(){return this.name}
-  public int getPrice(){return this.price}
-  public String getColor(){return this.color}
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;}
+  public String getColor(){return this.color;}
 }
